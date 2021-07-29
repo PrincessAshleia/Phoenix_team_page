7ed7e72c33ead1988d494f44f6bee55ae1958153
Ashley Anderson <ashley.anderson@gmail.com>
2021-07-29 18:39:17 -0400

    fixed typo in JavaScript

--- js/schedule.js
+++ js/schedule.js
@@ -1,19 +1,18 @@
-// When user clicks on the button
-toggle between hiding and showing dropdown content */
+// When user clicks on the button toggle between hiding and showing dropdown content
 function myFunction() {
-    document.getElementById("fullMenu").classList.toggle("show");
-  }
-  
-  // Close dropdown menu if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.menubtn')) {
-      var dropdowns = document.getElementsByClassName("menu-options");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
+  document.getElementById("fullMenu").classList.toggle("show");
+}
+
+// Close dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.menubtn')) {
+    var dropdowns = document.getElementsByClassName("menu-options");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
       }
     }
-  } 
+  }
+} 
