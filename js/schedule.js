7beb1f46c55b2f4cd8f9586d3ad0952f9cab436f
Ashley Anderson <ashley.anderson@gmail.com>
2021-07-29 18:24:18 -0400

    updated syntax

--- js/schedule.js
+++ js/schedule.js
@@ -1,13 +1,13 @@
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
+/* When user clicks on the button,
+toggle between hiding and showing dropdown content */
 function myFunction() {
     document.getElementById("fullMenu").classList.toggle("show");
   }
   
-  // Close the dropdown menu if the user clicks outside of it
+  // Close dropdown menu if the user clicks outside of it
   window.onclick = function(event) {
     if (!event.target.matches('.menubtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var dropdowns = document.getElementsByClassName("menu-options");
       var i;
       for (i = 0; i < dropdowns.length; i++) {
         var openDropdown = dropdowns[i];
